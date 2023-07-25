7b0d048c46c3e1f5354cab63002088feb1c14d6a
Marius <mj372012@gmail.com>
2023-07-25 21:28:56 +0300

    Updated DataProcessor Class Created frontend package and added WebApp.java file Created index.html file and added in resources folder

---
+++ src/main/java/frontend/WebApp.java
@@ -0,0 +1,71 @@
+
+
+package frontend;
+
+
+import org.example.DataProcessor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import static spark.Spark.*;
+
+public class WebApp {
+
+    public static void main(String[] args) {
+        // Set the port for Spark
+        port(8082);
+
+        // Serve the index.html page
+        get("/", (req, res) -> {
+            res.type("text/html");
+            return renderHTML();
+        });
+
+        // Define routes for the API and call the methods from DataProcessor class
+        DataProcessor dp = new DataProcessor();
+
+        get("/api/displayData", (req, res) -> displayData(dp));
+        get("/api/orderByReceivingCountry", (req, res) -> orderByReceivingCountry(dp));
+        get("/api/saveToDatabase", (req, res) -> saveToDatabase(dp));
+    }
+
+    private static String renderHTML() {
+        try {
+            // Read index.html content
+            InputStream inputStream = WebApp.class.getResourceAsStream("/index.html");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder htmlContent = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                htmlContent.append(line);
+            }
+            reader.close();
+
+            return htmlContent.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "<html><head></head><body><h1>Error: Failed to load HTML</h1></body></html>";
+        }
+    }
+
+
+    private static String displayData(DataProcessor dp) {
+        String result = dp.displayData();
+        return result;
+    }
+
+    private static String orderByReceivingCountry(DataProcessor dp) {
+        String result = dp.orderByReceivingCountry();
+        return result;
+    }
+
+    private static List<String> saveToDatabase(DataProcessor dp) {
+        List<String> results = dp.saveToDatabase();
+        return results;
+    }
+}
+
--- src/main/java/org/example/DataProcessor.java
+++ src/main/java/org/example/DataProcessor.java
@@ -5,77 +5,83 @@ import org.apache.spark.sql.SaveMode;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 
 public class DataProcessor {
 
-    //displays data from the Erasmus.csv file
-    public void displayData(){
-        //create spark session
-        SparkSession spark = SparkSession.builder()
+    private final SparkSession spark;
+
+    public DataProcessor() {
+        // Create a Spark session when the DataProcessor is initialized
+        this.spark = SparkSession.builder()
                 .appName("Erasmus")
                 .master("local[*]")
                 .getOrCreate();
 
-        //reads the Erasmus.csv file ,prints the schema and displays the data from the file.
+    }
+
+
+    // Close the Spark session when the DataProcessor is no longer needed
+    public void close() {
+        this.spark.stop();
+    }
+
+
+    //display data from the Erasmus.csv file
+    public String displayData() {
+
+        //prints the schema and displays the data from the file.
         DataFrameReader reader = spark.read();
         reader.option("header", "true");
 
         Dataset<Row> df = reader.csv("src/main/resources/Erasmus.csv");
         df.printSchema();
-        df.show(100,false); //Integer.Max to display the entire data in the file
+        df.show(100, false); //Integer.Max to display the entire data in the file
 
-        //stop spark session
-        spark.stop();
+        // Perform the data processing and collect the results into a list
+        List<String> results = df.toJSON().collectAsList();
+
+        return results.toString();
     }
 
 
+    //display filtered data from the Erasmus.csv file, ordered by receiving country code , sending country code and number of participants from each sending country
+    public String orderByReceivingCountry() {
 
-    //displays filtered data from the Erasmus.csv file, ordered by receiving country code , sending country code and number of participants from each sending country
-    public  void orderByReceivingCountry(){
-        //create spark session
-        SparkSession spark = SparkSession.builder()
-                .appName("Erasmus")
-                .master("local[*]")
-                .getOrCreate();
-
-        //reads the Erasmus.csv file
+        //read the Erasmus.csv file
         DataFrameReader reader = spark.read();
         reader.option("header", "true");
 
         Dataset<Row> df = reader.csv("src/main/resources/Erasmus.csv");
 
-        //Filters the receiving countries by country code
-        df=df.filter(functions.col("Receiving Country Code").isin("DE","IT","FR"));
+        //Filter the receiving countries by country code
+        df = df.filter(functions.col("Receiving Country Code").isin("DE", "IT", "FR"));
 
         Dataset<Row> groupedData = df.groupBy("Receiving Country Code", "Sending Country Code")
                 .agg(functions.count("Participant Age").alias("Number of Participants"))
                 .orderBy("Receiving Country Code", "Sending Country Code");
 
-        groupedData.show(100,false); //Integer.Max to display the entire data in the file
+        groupedData.show(100, false); //Integer.Max to display the entire data in the file
 
 
-        //stop spark session
-        spark.stop();
+        List<String> results = groupedData.toJSON().collectAsList();
+
+        return results.toString();
     }
 
-    // Saves data from the filtered countries to the database.
-    public  void saveToDatabase() {
+    // Save data from the filtered countries to the database.
+    public List<String> saveToDatabase() {
 
-        //creates spark session
-        SparkSession spark = SparkSession.builder()
-                .appName("Erasmus")
-                .master("local[*]")
-                .getOrCreate();
-
-        //Reads the Erasmus.csv file
+        //Read the Erasmus.csv file
         DataFrameReader reader = spark.read();
         reader.option("header", "true");
 
         Dataset<Row> df = reader.csv("src/main/resources/Erasmus.csv");
 
-        // Filters the data by the country code
+        // Filter the data by the country code
         df = df.filter(functions.col("Receiving Country Code").isin("DE", "FR", "IT"));
 
         Dataset<Row> groupedDataToBeSaved = df.groupBy("Receiving Country Code", "Sending Country Code")
@@ -83,26 +89,35 @@ public class DataProcessor {
                 .orderBy("Receiving Country Code", "Sending Country Code");
         //groupedDataToBeSaved.show(100, false);
 
-        //creates connection to the database
+        //Create connection to the database
         String jdbcUrl = "jdbc:mysql://localhost:3306/erasmus_mobility";
         Properties connectionProperties = new Properties();
         connectionProperties.setProperty("user", "root");
         connectionProperties.setProperty("password", loadPasswordFromConfigFile());
 
 
+        List<String> results = new ArrayList<>();
 
-        //saves the filtered data to the database
+        //Save the filtered data to the database
         String[] receivingCountries = {"DE", "FR", "IT"};
+        Dataset<Row> filteredData = null;
         for (String country : receivingCountries) {
-            Dataset<Row> filteredData = groupedDataToBeSaved.filter(functions.col("Receiving Country Code").equalTo(country));
-            String tableName = country.toLowerCase()+ "_table";
+            filteredData = groupedDataToBeSaved
+                    .filter(functions.col("Receiving Country Code").equalTo(country))
+                    .drop("Receiving Country Code");
+            String tableName = country.toLowerCase() + "_table";
             filteredData
                     .write()
                     .mode(SaveMode.Overwrite)
                     .jdbc(jdbcUrl, tableName, connectionProperties);
+
+            List<String> dataForCountry = filteredData.toJSON().collectAsList();///schimbat results cu dataForcountry
+
+            results.add("{\"country\": \"" + country + "\", \"data\": " + dataForCountry.toString() + "}");
+
         }
 
-        spark.stop();
+        return results;
 
     }
 
